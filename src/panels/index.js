83f50f0f866fad5a312cb8617f7904c1e8f404f7
BUBONDUDON <orelmarek@mail.ru>
2025-05-11 18:49:55 +0500

    Delete src directory

--- src/panels/index.js
+++
@@ -1,8 +0,0 @@
-//Странички основы
-export { HomePage } from './Pages/HomePage/HomePage'
-//Странички профиля
-export { ProfilePage } from './Pages/ProfilePage/ProfilePage'
-export { MyFav } from './Pages/ProfilePage/MyFav/MyFav';
-export { MyTests } from './Pages/ProfilePage/MyTests/MyTests';
-//...
-
